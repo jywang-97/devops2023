5d10d763dba038416a679617af05b810a584274d
jywang-97 <113971989+jywang-97@users.noreply.github.com>
2023-10-21 09:25:39 +0800

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -11,7 +11,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6514326552:AAF6xTqnDj-ph1dr9DMxvGk1NvsYbNov47s').build()
+    application = ApplicationBuilder().token('TOKEN').build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
